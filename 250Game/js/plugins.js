4ec225d0614e96d9060e055a5ac93f0e1b99e376
Sergey <sergeykhlynovskiy@gmail.com>
2023-01-21 23:23:11 -0700

    horse movement done. Gave up on highlighting

--- 250Game/js/plugins.js
+++ 250Game/js/plugins.js
@@ -4,6 +4,5 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"chess_movement","status":true,"description":"Basic movement and board display of chess pieces","parameters":{}},
 {"name":"DK_Mouse_System","status":true,"description":"v.1.2.0 [MV|MZ] Allows you to change the mouse cursor, activate events by clicking, hovering, etc.","parameters":{"defaultCursor":"","iconSynonyms":"[]","transferIcon":"0","destination":"{\"type\":\"default\",\"offsetX\":\"0\",\"offsetY\":\"0\",\"color\":\"white\",\"outlineColor\":\"default\",\"blendMode\":\"1\",\"scale\":\"1.00\",\"animation\":\"{\\\"bounceRate\\\":\\\"20\\\",\\\"minOpacity\\\":\\\"16\\\",\\\"maxOpacity\\\":\\\"192\\\",\\\"minScale\\\":\\\"0.15\\\",\\\"maxScale\\\":\\\"1.50\\\"}\"}"}}
 ];
