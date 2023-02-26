e14becab1bdd2166c4c643902d0ef699fe0bcd87
Ian <ian.gauk@paradocs.ca>
2023-02-26 14:36:18 -0700

    Rework chess JS to use notes

--- 250Game/js/plugins/chess.js
+++ 250Game/js/plugins/chess.js
@@ -1,93 +1,122 @@
 /*:
  * @author Sergey Khlynovskiy
- * @plugindesc chess movement
+ * @author Ian Gauk
+ * @plugindesc Controls chess movement
  * 
- * @help does chess stuff
+ * @help Provides functionality for a chess game, such as player movement and traps. Made specificially
+ * for the underpromotion chess game.
+
+ ###=========================================================================
+ ## Instructions
+ ###=========================================================================
+ Use event comments
+ 1. <chess:pawn>
+ Creates an enemy chess pawn, that attacks along the diagonal
+
+ 2. <chess:spike>
+ Creates a spike trap that cannot be stepped on while game switch 4 is ON
+
+ 3. <chess:pit>
+ Creates a tile that cannot be stepped on
+
+ 4. <chess:wall>
+ Creates an impassable wall
  *  
 */
 
-
 /**
  * Checks if a position coincides with a wall
- * @param mapInfo with information on wall placement
  * @param position [x,y] coordinates of position
  * @returns {boolean} true if
  */
-function isWall(mapInfo, position) {
-  const isWall = false;
-  if (mapInfo['wall']) {
-    return mapInfo['wall'].find((wall) => $gameMap.event(wall)._x === position[0] && $gameMap.event(wall)._y === position[1]);
-  }
-  return isWall;
+function isWall(position) {
+  return $gameMap.eventsXy(position[0], position[1])[0].isWall;
 }
+
+const LOAD_EVENT = 6;
+const SPIKE_ON_EVENT = 22;
 
 (function() {
   const params = PluginManager.parameters("chess");
 
-  // load from notes on map load
-  // TODO: load by tile id
-  const Chess_Game_On_Load = Game_Player.prototype.performTransfer;
-  Game_Player.prototype.performTransfer = function() {
-    Chess_Game_On_Load.call(this);
-    $gamePlayer.setThrough(false);
-    $gameVariables.setValue(7, {}); // clear map info game var
-    try {
-      let mapInfo = JSON.parse($dataMap.note.split('/')[0]);
-      $gameVariables.setValue(7, mapInfo);
-    } catch (e) {
-      console.log(e);
+  const Chess_Setup_Page = Game_Event.prototype.setupPage;
+  Game_Event.prototype.setupPage = function() {
+    Chess_Setup_Page.apply(this, arguments);
+    this.setupChess();
+  };
+
+  Game_Event.prototype.setupChess = function() {
+    if (this._erased) {
+      return;
     }
+
+    const comments = this.getComments();
+    comments.forEach((comment) => {
+      if (comment.match(/<chess:wall>/i)) {
+        this.isWall = true;
+      } else if (comment.match(/<chess:spike>/i)) {
+        this.isSpike = true;
+      } else if (comment.match(/<chess:pit>/i)) {
+        this.pit = { isActivated: false }
+      } else if (comment.match(/<chess:pawn>/i)) {
+        this.isPawn = true;
+      }
+    });
   };
 
 
-  const Chess_Game_Event_updateEvents = Game_Map.prototype.updateEvents;
+  const Chess_Update_Events = Game_Map.prototype.updateEvents;
   Game_Map.prototype.updateEvents = function() {
-    Chess_Game_Event_updateEvents.apply(this, arguments);
-    if (!$gameSwitches.value(2)) {
-      this.checkEat();
-    }
+    Chess_Update_Events.apply(this, arguments);
+    this.updateChessEvents.apply(this, arguments);
   };
 
-  Game_Map.prototype.checkEat = function() {
-    if (!$gamePlayer.isMoveRouteForcing()) {
-      const playerLoc = [$gamePlayer.x, $gamePlayer.y];
-      // a little gross
-      const mapInfo = $gameVariables.value(7);
-      for (const [enemy, ids] of Object.entries(mapInfo)) {
-        let route = null;
+  Game_Map.prototype.updateChessEvents = function() {
+    const gameMap = this;
+    this.events().forEach(event => {
+      const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+      const touchingPlayer = event.x === playerCoordinates[0] && event.y === playerCoordinates[1];
+      if (!!event.pit && !event.pit.isActivated && event.isTouchingPlayer && !touchingPlayer) {
+        // true if the player was previously on this event but is moving away
+        // TODO: play sound effect
+        $gameSelfSwitches.setValue([this.mapId(), event._eventId, 'A'], true);
+        event.pit.isActivated = true;
+      }
+      event.isTouchingPlayer = touchingPlayer;
+      if (event.isSpike && event.isTouchingPlayer && $gameSwitches.value(SPIKE_ON_EVENT)) {
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+      } else if (!!event.pit && event.isTouchingPlayer && event.pit.isActivated) {
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+      } else if (!!event.pit && event.isTouchingPlayer && event.pit.isActivated) {
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+      } else if (event.isPawn) {
+        const pawnRoute = this.checkEatPawn([event.x, event.y], playerCoordinates);
+        if (pawnRoute) {
+          this.pawnAttack(event, pawnRoute);
+        }
+      }
+    });
+  }
 
-        ids.forEach((id) => {
-
-          const enemyLoc = [$gameMap.event(id).x, $gameMap.event(id).y];
-          if (enemy === "pawn") {
-            route = this.checkEatPawn(enemyLoc, playerLoc);
-          }
-
-
-          if (route) {
-            $gameMap.event(id).forceMoveRoute(route);
-            $gameParty.members()[0].setHp(0) //Carlin: Kills player, Game Over
-            // TODO: game end
-            //this._interpreter.setWaitMode('route');
-          }
-
-          if (enemy === "spike" && enemyLoc[0] === playerLoc[0] && enemyLoc[1] === playerLoc[1] && $gameSwitches.value(4) == true) {
-            $gameParty.members()[0].setHp(0)
-          }
-
-          if (enemy === "pit" && enemyLoc[0] === playerLoc[0] && enemyLoc[1] === playerLoc[1]) {
-            $gameParty.members()[0].setHp(0)
-          }
-        })
+  Game_Map.prototype.pawnAttack = function(event, pawnRoute) {
+    event.forceMoveRoute(pawnRoute);
+    while (event.isMoveRouteForcing()) {
+      const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+      let playerIsTouchingEvent = event.x === playerCoordinates[0] && event.y === playerCoordinates[1];
+      if (playerIsTouchingEvent) {
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+        break;
       }
     }
   }
 
-  // return a route for pawn eating if possible otherwise null
-  Game_Map.prototype.checkEatPawn = function(pawnStart, pawnEnd) {
-    if (pawnEnd[0] == pawnStart[0]-1 && pawnEnd[1] == pawnStart[1] + 1) {
+  /**
+   * Return a route for pawn eating if possible otherwise null
+   */
+  Game_Map.prototype.checkEatPawn = function (pawnStart, pawnEnd) {
+    if (pawnEnd[0] === pawnStart[0]-1 && pawnEnd[1] === pawnStart[1] + 1) {
       return {list: [{code: Game_Character.ROUTE_MOVE_LOWER_L}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
-    } else if (pawnEnd[0] == pawnStart[0]+1 && pawnEnd[1] == pawnStart[1] + 1) {
+    } else if (pawnEnd[0] === pawnStart[0]+1 && pawnEnd[1] === pawnStart[1] + 1) {
       return {list: [{code: Game_Character.ROUTE_MOVE_LOWER_R}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
     } else {
       return null;
@@ -100,10 +129,8 @@ function isWall(mapInfo, position) {
    * @param requestCoordinates requested movement position [x, y]
    */
   Game_Map.prototype.bishopRoute = function(startCoordinates, requestCoordinates) {
-    const mapInfo = $gameVariables.value(7);
-
     let canMove = (Math.abs(startCoordinates[0] - requestCoordinates[0]) === Math.abs(startCoordinates[1] - requestCoordinates[1]))
-        && !isWall(mapInfo, requestCoordinates);
+        && !isWall(requestCoordinates);
 
     if (canMove) {
       const horizontalMovement = requestCoordinates[0] - startCoordinates[0];
@@ -137,10 +164,8 @@ function isWall(mapInfo, position) {
    * @param requestCoordinates requested movement position [x, y]
    */
   Game_Map.prototype.rookRoute = function(startCoordinates, requestCoordinates) {
-    const mapInfo = $gameVariables.value(7);
-
     let canMove = (startCoordinates[0] === requestCoordinates[0] || startCoordinates[1] === requestCoordinates[1])
-        && !isWall(mapInfo, requestCoordinates);
+        && !isWall(requestCoordinates);
 
     if (canMove) {
       const horizontalMovement = requestCoordinates[0] - startCoordinates[0];
@@ -171,7 +196,7 @@ function isWall(mapInfo, position) {
   // return a route for the horse if possible otherwise null
   Game_Map.prototype.horseRoute = function(horseStart, horseEnd) {
     console.log(horseStart, horseEnd);
-    const mapInfo = $gameVariables.value(7);
+    const mapInfo = $gameVariables.value(15);
     const possiblePos = [
       [horseStart[0]-2, horseStart[1]+1],
       [horseStart[0]-2, horseStart[1]-1],
@@ -188,7 +213,7 @@ function isWall(mapInfo, position) {
 
     for (let i = 0; i < possiblePos.length; i++) {
       if (possiblePos[i].toString() === horseEnd.toString()) {
-        canGo = !isWall(mapInfo, horseEnd);
+        canGo = !isWall(horseEnd);
         // setting routes. very gross i know so if you have a better idea lmk
         if (canGo) {
           switch (i) {
@@ -223,12 +248,5 @@ function isWall(mapInfo, position) {
       }
     }
     return route;
-  }
-
-  // here we can get rid of enemies so an invisible pawn can't eat the player
-  Game_Map.prototype.removeMapInfo = function(info, id) {
-    const mapInfo = $gameVariables.value(7);
-    mapInfo[info] = mapInfo[info].filter((v, i, arr) => v != id);
-    $gameVariables.setValue(7, mapInfo);
   }
 })();
