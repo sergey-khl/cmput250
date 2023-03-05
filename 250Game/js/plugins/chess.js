314d59c4e9a7f00f1749b9653ec5f1bb8054c7b3
Ian <ian.gauk@paradocs.ca>
2023-03-05 16:34:38 -0700

    Conveyor belt, sound effects, movement clean up

--- 250Game/js/plugins/chess.js
+++ 250Game/js/plugins/chess.js
@@ -13,7 +13,7 @@
  1. <chess:pawn>
  Creates an enemy chess pawn, that attacks along the diagonal
 
- 2. <chess:spike>
+ 2. <chess:spike(:opp)>
  Creates a spike trap that cannot be stepped on while game switch 4 is ON
 
  3. <chess:pit>
@@ -21,8 +21,45 @@
 
  4. <chess:wall>
  Creates an impassable wall
+
+ 5. <chess:conveyor:DIRECTION>
+ DIRECTION is one of (left/right/up/down)
+ Creates a tile that pushes the player in the provided direction
  *  
 */
+
+
+// --- SWITCHES --- //
+const LOAD_EVENT = 6;
+const SPIKE_ON_EVENT = 22;
+const HORSE_SWITCH = 18;
+const BISHOP_SWITCH = 20;
+const ROOK_SWITCH = 19;
+
+// --- HOVER --- //
+const HOVER_ICON = 31;
+const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
+
+// --- SOUND EFFECTS --- //
+const SPIKE_DEATH_SE_NAME = "spikeDEATH";
+const SPIKE_DEATH_SE = { name: SPIKE_DEATH_SE_NAME, volume: 100, pitch: 100 };
+const CHESS_MOVE_SE_1_NAME = "chessMOVEMENT";
+const CHESS_MOVE_SE_2_NAME = "chessMOVEMENT2";
+const CHESS_MOVE_SE_3_NAME = "chessMOVEMENT3";
+const CHESS_MOVEMENT_SE_1 = {name: CHESS_MOVE_SE_1_NAME, volume: 90, pitch: 100};
+const CHESS_MOVEMENT_SE_2 = {name: CHESS_MOVE_SE_2_NAME, volume: 90, pitch: 100};
+const CHESS_MOVEMENT_SE_3 = {name: CHESS_MOVE_SE_3_NAME, volume: 90, pitch: 100};
+const PIT_OPEN_SE_NAME = "pitOPEN";
+const PIT_OPEN_SE = { name: PIT_OPEN_SE_NAME, volume: 100, pitch: 110 };
+const PIT_FALL_SE_NAME = "pitFALL";
+const PIT_FALL_SE = { name: PIT_FALL_SE_NAME, volume: 100, pitch: 100 };
+
+
+// --- MOVEMENT --- //
+const UP = Game_Character.ROUTE_MOVE_UP;
+const DOWN = Game_Character.ROUTE_MOVE_DOWN;
+const LEFT = Game_Character.ROUTE_MOVE_LEFT;
+const RIGHT = Game_Character.ROUTE_MOVE_RIGHT;
 
 /**
  * Checks if a position coincides with a wall
@@ -32,14 +69,6 @@
 function isWall(position) {
   return !!$gameMap.eventsXy(position[0], position[1])[0].isWall;
 }
-
-const LOAD_EVENT = 6;
-const SPIKE_ON_EVENT = 22;
-const HORSE_SWITCH = 18;
-const BISHOP_SWITCH = 20;
-const ROOK_SWITCH = 19;
-const HOVER_ICON = 31;
-const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
 
 (function() {
   const params = PluginManager.parameters("chess");
@@ -65,14 +94,24 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
         this.spike = { opposite: true }
       } else if (comment.match(/<chess:pit>/i)) {
         this.pit = { isActivated: false }
-      } else if (comment.match(/<chess:pawn>/i)) {
-        this.isPawn = true;
+      } else if (comment.match(/<chess:conveyor:left>/i)) {
+        this.conveyor = { direction: LEFT }
+      } else if (comment.match(/<chess:conveyor:right>/i)) {
+        this.conveyor = { direction: RIGHT }
+      } else if (comment.match(/<chess:conveyor:up>/i)) {
+        this.conveyor = { direction: UP }
+      } else if (comment.match(/<chess:conveyor:down>/i)) {
+        this.conveyor = { direction: DOWN }
       }
     });
   };
 
   Game_Event.prototype.isSpike = function() {
     return !!this.spike;
+  }
+
+  Game_Event.prototype.isConveyorBelt = function() {
+    return !!this.conveyor;
   }
 
   const Chess_Update_Events = Game_Map.prototype.updateEvents;
@@ -90,53 +129,33 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
       const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
       const touchingPlayer = event.x === playerCoordinates[0] && event.y === playerCoordinates[1];
       if (!!event.pit && !event.pit.isActivated && event.isTouchingPlayer && !touchingPlayer) {
-        // true if the player was previously on this event but is moving away
-        // TODO: play sound effect
+        AudioManager.playSe(PIT_OPEN_SE);
         $gameSelfSwitches.setValue([this.mapId(), event._eventId, 'A'], true);
         event.pit.isActivated = true;
       }
       event.isTouchingPlayer = touchingPlayer;
       if (event.isSpike() && event.isTouchingPlayer && spikeOn(event)) {
+        AudioManager.playSe(SPIKE_DEATH_SE);
         $gameTemp.reserveCommonEvent(LOAD_EVENT);
       } else if (!!event.pit && event.isTouchingPlayer && event.pit.isActivated) {
+        AudioManager.playSe(PIT_FALL_SE);
         $gameTemp.reserveCommonEvent(LOAD_EVENT);
-      } else if (!!event.pit && event.isTouchingPlayer && event.pit.isActivated) {
-        $gameTemp.reserveCommonEvent(LOAD_EVENT);
-      } else if (event.isPawn) {
-        const pawnRoute = this.checkEatPawn([event.x, event.y], playerCoordinates);
-        if (pawnRoute) {
-          this.pawnAttack(event, pawnRoute);
-        }
+      } else if (event.isTouchingPlayer && event.isConveyorBelt() && !$gamePlayer.isMoveRouteForcing()) {
+        event.activateConveyorBelt();
       }
     });
   }
 
-  // TODO: fix this, it does not actually work...
-  Game_Map.prototype.pawnAttack = function(event, pawnRoute) {
-    event.forceMoveRoute(pawnRoute);
-    while (event.isMoveRouteForcing()) {
-      const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
-      let playerIsTouchingEvent = event.x === playerCoordinates[0] && event.y === playerCoordinates[1];
-      if (playerIsTouchingEvent) {
-        $gameTemp.reserveCommonEvent(LOAD_EVENT);
-        break;
-      }
-    }
+  Game_Event.prototype.activateConveyorBelt = function() {
+    const route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}], repeat: false, skippable: false};
+    route.list.push({code: this.conveyor.direction});
+    route.list.push({code: Game_Character.ROUTE_THROUGH_OFF});
+    route.list.push({code: Game_Character.ROUTE_END});
+    $gamePlayer.forceMoveRoute(route);
+    $gameMap._interpreter.setWaitMode("route");
   }
 
-  /**
-   * Return a route for pawn eating if possible otherwise null
-   */
-  Game_Map.prototype.checkEatPawn = function (pawnStart, pawnEnd) {
-    if (pawnEnd[0] === pawnStart[0]-1 && pawnEnd[1] === pawnStart[1] + 1) {
-      return {list: [{code: Game_Character.ROUTE_MOVE_LOWER_L}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
-    } else if (pawnEnd[0] === pawnStart[0]+1 && pawnEnd[1] === pawnStart[1] + 1) {
-      return {list: [{code: Game_Character.ROUTE_MOVE_LOWER_R}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
-    } else {
-      return null;
-    }
-  }
-
+  // ---------------------------------------- CHESS HIGHLIGHT ---------------------------------------- //
   Game_Character.prototype.highlightChessMoves = function() {
     let highlightFunction;
     if ($gameSwitches.value(HORSE_SWITCH)) { highlightFunction = this.highlightKnight; }
@@ -205,23 +224,34 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
     $gamePlayer.highlightChessMoves();
   }
 
-  const Chess_Route_End = Game_Character.prototype.processRouteEnd;
-  Game_Character.prototype.processRouteEnd = function() {
-    Chess_Route_End.call(this);
-    this.highlightChessMoves();
-  }
 
+  // ---------------------------------------- CHESS MOVEMENT ---------------------------------------- //
   Game_Character.prototype.moveChess = function(destinationCoordinates) {
     let movementFunction;
     if ($gameSwitches.value(HORSE_SWITCH)) {  movementFunction = this.moveKnight; }
     else if ($gameSwitches.value(ROOK_SWITCH)) {movementFunction = this.moveRook; }
     else if ($gameSwitches.value(BISHOP_SWITCH)) { movementFunction = this.moveBishop; }
     if (movementFunction) {
-      movementFunction.call(this, destinationCoordinates);
-      return true;
+      return movementFunction.call(this, destinationCoordinates);
     } else {
       return false;
     }
+  }
+
+  Game_Player.prototype.playChessMovementSE = function() {
+    const movementSoundEffects = [
+      CHESS_MOVEMENT_SE_1,
+      CHESS_MOVEMENT_SE_2,
+      CHESS_MOVEMENT_SE_3]
+    let randomSEIndex = Math.floor(Math.random() * 3);
+    AudioManager.playSe(movementSoundEffects[randomSEIndex]);
+  }
+
+  const Chess_Route_End = Game_Player.prototype.processRouteEnd;
+  Game_Player.prototype.processRouteEnd = function() {
+    Chess_Route_End.call(this);
+    this.highlightChessMoves();
+    this.playChessMovementSE();
   }
 
   /**
@@ -236,7 +266,7 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
       const horizontalMovement = requestCoordinates[0] - this.x;
       const verticalMovement = this.y - requestCoordinates[1]; // RPG Maker 0,0 top left
 
-      const route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}], repeat: false, skippable: false};
+      const route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}], repeat: false, skippable: false};
       let routeCode;
       if (horizontalMovement > 0 && verticalMovement > 0) {
         routeCode = Game_Character.ROUTE_MOVE_UPPER_R;
@@ -247,14 +277,13 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
       } else if (horizontalMovement > 0 && verticalMovement < 0) {
         routeCode = Game_Character.ROUTE_MOVE_LOWER_R;
       }
-      const finalizeRouteElements = [{code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}];
       for (let i = 0; i < Math.abs(verticalMovement); i++) {
         route.list.push({code: routeCode})
       }
 
-      route.list = route.list.concat(finalizeRouteElements);
+      route.list.push({code: Game_Character.ROUTE_THROUGH_OFF});
+      route.list.push({code: Game_Character.ROUTE_END});
       this.forceMoveRoute(route);
-      $gameMap._interpreter.setWaitMode("route");
       return true;
     }
     return false;
@@ -272,7 +301,7 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
       const horizontalMovement = requestCoordinates[0] - this.x;
       const verticalMovement = this.y - requestCoordinates[1]; // RPG Maker 0,0 top left
 
-      const route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}], repeat: false, skippable: false};
+      const route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}], repeat: false, skippable: false};
       let routeCode;
       if (verticalMovement && verticalMovement > 0) {
         routeCode = Game_Character.ROUTE_MOVE_UP;
@@ -283,14 +312,13 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
       } else if (horizontalMovement && horizontalMovement < 0) {
         routeCode = Game_Character.ROUTE_MOVE_LEFT;
       }
-      const finalizeRouteElements = [{code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}];
       for (let i = 0; i < Math.max(Math.abs(verticalMovement), Math.abs(horizontalMovement)); i++) {
         route.list.push({code: routeCode})
       }
 
-      route.list = route.list.concat(finalizeRouteElements);
+      route.list.push({code: Game_Character.ROUTE_THROUGH_OFF});
+      route.list.push({code: Game_Character.ROUTE_END});
       this.forceMoveRoute(route);
-      $gameMap._interpreter.setWaitMode("route");
       return true;
     }
     return false;
@@ -315,36 +343,34 @@ const HOVER_ICON_COMMENT = "<hover_icon:" + HOVER_ICON + ">";
     for (let i = 0; i < possiblePos.length; i++) {
       if (possiblePos[i].toString() === horseEnd.toString()) {
         canGo = !isWall(horseEnd);
-        // setting routes. very gross i know so if you have a better idea lmk
         if (canGo) {
           switch (i) {
             case 0:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 1:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 2:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 3:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_LEFT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 4:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 5:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 6:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_DOWN}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
             case 7:
-              route = {list: [{code: Game_Character.ROUTE_SWITCH_ON, parameters: [2]}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_SWITCH_OFF, parameters: [2]}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
+              route = {list: [{code: Game_Character.ROUTE_THROUGH_ON}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_RIGHT}, {code: Game_Character.ROUTE_MOVE_UP}, {code: Game_Character.ROUTE_THROUGH_OFF}, {code: Game_Character.ROUTE_END}], repeat: false, skippable: false};
               break;
           }
           this.forceMoveRoute(route);
-          $gameMap._interpreter.setWaitMode("route");
           return true;
         }
         canGo = true;
