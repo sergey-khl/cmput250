3231efa9d1914b1ff72311fabc4d1806c2f4f692
Ian <ian.gauk@paradocs.ca>
2023-03-06 11:24:25 -0700

    chess.js clean up, added flame traps, fixed rook and bishop movement

--- 250Game/js/plugins/chess.js
+++ 250Game/js/plugins/chess.js
@@ -53,7 +53,16 @@ const PIT_OPEN_SE_NAME = "pitOPEN";
 const PIT_OPEN_SE = { name: PIT_OPEN_SE_NAME, volume: 100, pitch: 110 };
 const PIT_FALL_SE_NAME = "pitFALL";
 const PIT_FALL_SE = { name: PIT_FALL_SE_NAME, volume: 100, pitch: 100 };
+const FIRE_BURN_SE_NAME = "fireBURN";
+const FIRE_BURN_SE = { name: FIRE_BURN_SE_NAME, volume: 100, pitch: 100 };
+const FIRE_TIGGERED_SE_NAME = "flameTRIGGERED";
+const FIRE_TRIGGERED_SE = { name: FIRE_TIGGERED_SE_NAME, volume: 70, pitch: 110 };
+const FLAME_ACTIVE_SE_NAME = "flameACTIVE";
+const FLAME_ACTIVE_SE = { name: FLAME_ACTIVE_SE_NAME, volume: 70, pitch: 110 };
 
+// --- IMAGES --- //
+const SUN_FLARE_IMAGE = {"tileId": 0, "characterName": "!Flame", "direction": 3, "pattern": 0, "characterIndex": 6};
+const FIRE_IMAGE = {"tileId": 0, "characterName": "!Other2", "direction": 1, "pattern": 0, "characterIndex": 3};
 
 // --- MOVEMENT --- //
 const UP = Game_Character.ROUTE_MOVE_UP;
@@ -70,8 +79,26 @@ function isWall(position) {
   return !!$gameMap.eventsXy(position[0], position[1])[0].isWall;
 }
 
+function getRandomInt(max, min = 0) {
+  return Math.floor(Math.random() * max) + min;
+}
+
 (function() {
   const params = PluginManager.parameters("chess");
+
+  let tickCounter = 0;
+  const flameEvents = [];
+  const wallEvents = [];
+  const spikeEvents = [];
+  const pitEvents = [];
+  const conveyorBeltEvents = [];
+  Game_Map.prototype.flames = function() { return flameEvents };
+  Game_Map.prototype.walls = function() { return wallEvents };
+  Game_Map.prototype.pits = function() { return pitEvents };
+  Game_Map.prototype.spikes = function() { return spikeEvents };
+  Game_Map.prototype.conveyors = function() { return conveyorBeltEvents };
+
+
 
   const Chess_Setup_Page = Game_Event.prototype.setupPage;
   Game_Event.prototype.setupPage = function() {
@@ -88,20 +115,48 @@ function isWall(position) {
     comments.forEach((comment) => {
       if (comment.match(/<chess:wall>/i)) {
         this.isWall = true;
+        $gameMap.walls().push(this);
       } else if (comment.match(/<chess:spike>/i)) { // TODO: add timing here instead of in event
         this.spike = { opposite: false };
+        $gameMap.spikes().push(this);
       } else if (comment.match(/<chess:spike:opp>/i)) {
         this.spike = { opposite: true }
+        $gameMap.spikes().push(this);
       } else if (comment.match(/<chess:pit>/i)) {
         this.pit = { isActivated: false }
+        $gameMap.pits().push(this);
       } else if (comment.match(/<chess:conveyor:left>/i)) {
         this.conveyor = { direction: LEFT }
+        $gameMap.conveyors().push(this);
       } else if (comment.match(/<chess:conveyor:right>/i)) {
         this.conveyor = { direction: RIGHT }
+        $gameMap.conveyors().push(this);
       } else if (comment.match(/<chess:conveyor:up>/i)) {
         this.conveyor = { direction: UP }
+        $gameMap.conveyors().push(this);
       } else if (comment.match(/<chess:conveyor:down>/i)) {
         this.conveyor = { direction: DOWN }
+        $gameMap.conveyors().push(this);
+      } else if (comment.match(/<chess:flame>/i)) {
+        this.flame = {
+          /**
+           * Dictates if the flame is currently on fire
+           */
+          active: false,
+          /**
+           * Dictates if the flame will soon become active
+           */
+          triggered: false,
+          /**
+           * True when the flame is in the process of activating
+           */
+          activating: false,
+          /**
+           * True when a flame is in the process of triggering
+           */
+          triggering: false
+        };
+        $gameMap.flames().push(this);
       }
     });
   };
@@ -114,6 +169,10 @@ function isWall(position) {
     return !!this.conveyor;
   }
 
+  Game_Event.prototype.isFlame = function() {
+    return !!this.flame;
+  }
+
   const Chess_Update_Events = Game_Map.prototype.updateEvents;
   Game_Map.prototype.updateEvents = function() {
     Chess_Update_Events.apply(this, arguments);
@@ -124,26 +183,113 @@ function isWall(position) {
     return event.spike.opposite ? !$gameSwitches.value(SPIKE_ON_EVENT) : $gameSwitches.value(SPIKE_ON_EVENT);
   }
 
-  Game_Map.prototype.updateChessEvents = function() {
-    this.events().forEach(event => {
-      const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
-      const touchingPlayer = event.x === playerCoordinates[0] && event.y === playerCoordinates[1];
-      if (!!event.pit && !event.pit.isActivated && event.isTouchingPlayer && !touchingPlayer) {
-        AudioManager.playSe(PIT_OPEN_SE);
-        $gameSelfSwitches.setValue([this.mapId(), event._eventId, 'A'], true);
-        event.pit.isActivated = true;
+  Game_Map.prototype.updateFlameActivations = function() {
+    if (!flameEvents.some(event => event.flame.triggered || event.flame.triggering)) { // randomly select the next flame to activate
+      const unTriggeredFlames = this.flames().filter(event => !event.flame.triggered && !event.flame.triggering && !event.flame.active && !event.flame.activating);
+      if (unTriggeredFlames.length) {
+        unTriggeredFlames[getRandomInt(unTriggeredFlames.length)].triggerFlame();
       }
-      event.isTouchingPlayer = touchingPlayer;
-      if (event.isSpike() && event.isTouchingPlayer && spikeOn(event)) {
+    }
+    const flamesToActivate = this.flames().filter(event => event.flame.triggered && !event.flame.active && !event.flame.activating);
+    flamesToActivate.forEach(event => event.activateFlame());
+  }
+
+  Game_Event.prototype.triggerFlame = function() {
+    this.flame.triggering = true;
+    setTimeout(() => {
+      this.setImage(SUN_FLARE_IMAGE.characterName, SUN_FLARE_IMAGE.characterIndex);
+      this.flame.triggered = true;
+      this.flame.triggering = false;
+      AudioManager.playSe(FIRE_TRIGGERED_SE);
+    }, getRandomInt(3000, 1000));
+  }
+
+  Game_Event.prototype.activateFlame = function() {
+    this.flame.activating = true;
+    setTimeout(() => {
+      this.flame.triggered = false;
+      this.flame.active = true;
+      AudioManager.stopSe()
+      AudioManager.playSe(FLAME_ACTIVE_SE);
+      this.setImage(FIRE_IMAGE.characterName, FIRE_IMAGE.characterIndex);
+      setTimeout(() => {
+        this.setImage('', 0);
+        this.flame.active = false;
+        this.flame.activating = false;
+        this.setTileImage(SUN_FLARE_IMAGE.characterName, 3, 5);
+      }, 3000);
+    }, 3000);
+  }
+
+  Game_Map.prototype.checkSpikeDeath = function() {
+    const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+
+    this.spikes().forEach(spikeEvent => {
+      spikeEvent.isTouchingPlayer = spikeEvent.x === playerCoordinates[0] && spikeEvent.y === playerCoordinates[1];
+
+      if (spikeEvent.isTouchingPlayer && spikeOn(spikeEvent)) {
         AudioManager.playSe(SPIKE_DEATH_SE);
         $gameTemp.reserveCommonEvent(LOAD_EVENT);
-      } else if (!!event.pit && event.isTouchingPlayer && event.pit.isActivated) {
-        AudioManager.playSe(PIT_FALL_SE);
-        $gameTemp.reserveCommonEvent(LOAD_EVENT);
-      } else if (event.isTouchingPlayer && event.isConveyorBelt() && !$gamePlayer.isMoveRouteForcing()) {
-        event.activateConveyorBelt();
       }
     });
+  }
+
+  Game_Map.prototype.checkConveyorBeltMovement = function() {
+    const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+
+    this.conveyors().forEach(conveyorBeltEvent => {
+      conveyorBeltEvent.isTouchingPlayer = conveyorBeltEvent.x === playerCoordinates[0] && conveyorBeltEvent.y === playerCoordinates[1];
+
+      if (conveyorBeltEvent.isTouchingPlayer && !$gamePlayer.isMoveRouteForcing()) {
+        conveyorBeltEvent.activateConveyorBelt();
+      }
+    });
+  }
+
+  Game_Map.prototype.checkFlameDeath = function() {
+    const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+
+    this.flames().forEach(flameEvent => {
+      flameEvent.isTouchingPlayer = flameEvent.x === playerCoordinates[0] && flameEvent.y === playerCoordinates[1];
+
+      if (flameEvent.isTouchingPlayer && flameEvent.flame.active) {
+        AudioManager.playSe(FIRE_BURN_SE);
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+      }
+    });
+  }
+
+  Game_Map.prototype.updatePitTraps = function() {
+    this.pits().forEach(pitEvent => {
+      const playerCoordinates = [$gamePlayer.x, $gamePlayer.y];
+      const touchingPlayer = pitEvent.x === playerCoordinates[0] && pitEvent.y === playerCoordinates[1];
+
+      if  (!pitEvent.pit.isActivated && pitEvent.isTouchingPlayer && !touchingPlayer) {
+        AudioManager.playSe(PIT_OPEN_SE);
+        $gameSelfSwitches.setValue([this.mapId(), pitEvent._eventId, 'A'], true);
+        pitEvent.pit.isActivated = true;
+      }
+
+      pitEvent.isTouchingPlayer = touchingPlayer;
+
+      if (pitEvent.isTouchingPlayer && pitEvent.pit.isActivated) {
+        AudioManager.playSe(PIT_FALL_SE);
+        $gameTemp.reserveCommonEvent(LOAD_EVENT);
+        $gameSelfSwitches.setValue([this.mapId(), pitEvent._eventId, 'A'], false);
+      }
+    })
+  }
+
+  Game_Map.prototype.updateChessEvents = function() {
+    tickCounter++;
+    if (tickCounter % 50 === 0) {
+      this.updateFlameActivations();
+    }
+
+    this.checkSpikeDeath();
+    this.checkConveyorBeltMovement();
+    this.checkFlameDeath();
+    this.updatePitTraps();
   }
 
   Game_Event.prototype.activateConveyorBelt = function() {
@@ -152,7 +298,6 @@ function isWall(position) {
     route.list.push({code: Game_Character.ROUTE_THROUGH_OFF});
     route.list.push({code: Game_Character.ROUTE_END});
     $gamePlayer.forceMoveRoute(route);
-    $gameMap._interpreter.setWaitMode("route");
   }
 
   // ---------------------------------------- CHESS HIGHLIGHT ---------------------------------------- //
@@ -163,6 +308,10 @@ function isWall(position) {
     else if ($gameSwitches.value(BISHOP_SWITCH)) { highlightFunction = this.highlightBishop; }
     if (highlightFunction) { highlightFunction.call(this) }
   }
+
+  // Game_Map.prototype.removeAllChessHighlight = function() {
+  //   this.events().forEach(event => removeChessHighlight(event));
+  // }
 
   Game_Character.prototype.isValidBishopEvent = function(event) {
     return (Math.abs(this.x - event.x) === Math.abs(this.y - event.y)) && !event.isWall
@@ -224,6 +373,19 @@ function isWall(position) {
     $gamePlayer.highlightChessMoves();
   }
 
+  // const Chess_Start_Message = Window_Message.prototype.startMessage;
+  // Window_Message.prototype.startMessage = function() {
+  //   $gameMap.removeAllChessHighlight();
+  //   Chess_Start_Message.call(this);
+  // }
+  //
+  // const Chess_Terminate_Message = Window_Message.prototype.terminateMessage;
+  // Window_Message.prototype.terminateMessage = function() {
+  //   Chess_Terminate_Message.call(this);
+  //   $gamePlayer.highlightChessMoves();
+  // }
+
+
 
   // ---------------------------------------- CHESS MOVEMENT ---------------------------------------- //
   Game_Character.prototype.moveChess = function(destinationCoordinates) {
@@ -242,8 +404,9 @@ function isWall(position) {
     const movementSoundEffects = [
       CHESS_MOVEMENT_SE_1,
       CHESS_MOVEMENT_SE_2,
-      CHESS_MOVEMENT_SE_3]
-    let randomSEIndex = Math.floor(Math.random() * 3);
+      CHESS_MOVEMENT_SE_3
+    ];
+    let randomSEIndex = getRandomInt(3);
     AudioManager.playSe(movementSoundEffects[randomSEIndex]);
   }
 
@@ -254,13 +417,52 @@ function isWall(position) {
     this.playChessMovementSE();
   }
 
+  Game_Character.prototype.bishopCanMove = function(requestCoordinates) {
+    if ((Math.abs(this.x - requestCoordinates[0]) !== Math.abs(this.y - requestCoordinates[1])) || isWall(requestCoordinates)) {
+      return false;
+    }
+
+    function isValidBishopMove() {
+      return event => Math.abs(this.x - event.x) === Math.abs(this.y - event.y);
+    }
+
+    if (this.x > requestCoordinates[0] && this.y > requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidBishopMove.call(this)).some(event => (event.x < this.x && event.x > requestCoordinates[0]) && (event.y < this.y && event.y > requestCoordinates[1]));
+    } else if (this.x < requestCoordinates[0] && this.y < requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidBishopMove.call(this)).some(event => (event.x > this.x && event.x < requestCoordinates[0]) && (event.y > this. y && event.y < requestCoordinates[1]));
+    } else if (this.x > requestCoordinates[0] && this.y < requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidBishopMove.call(this)).some(event => (event.x < this.x && event.x > requestCoordinates[0]) && (event.y > this.y && event.y < requestCoordinates[1]));
+    } else if (this.x < requestCoordinates[0] && this.y > requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidBishopMove.call(this)).some(event => (event.x > this.x && event.x < requestCoordinates[0]) && (event.y < this.y && event.y > requestCoordinates[1]))
+    }
+  }
+
+  Game_Character.prototype.rookCanMove = function(requestCoordinates) {
+    if ((this.x !== requestCoordinates[0] && this.y !== requestCoordinates[1]) || isWall(requestCoordinates)) {
+      return false;
+    }
+
+    function isValidRookMove() {
+      return event => this.x === event.x || this.y === event.y;
+    }
+
+    if (this.x < requestCoordinates[0]) {
+      return !$gameMap.walls().filter(isValidRookMove.call(this)).some(event => event.x > this.x && event.x < requestCoordinates[0]);
+    } else if (this.x > requestCoordinates[0]) {
+      return !$gameMap.walls().filter(isValidRookMove.call(this)).some(event => event.x < this.x && event.x > requestCoordinates[0]);
+    } else if (this.y < requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidRookMove.call(this)).some(event => event.y > this.y && event.y < requestCoordinates[1]);
+    } else if (this.y > requestCoordinates[1]) {
+      return !$gameMap.walls().filter(isValidRookMove.call(this)).some(event => event.y < this.y && event.y > requestCoordinates[1]);
+    }
+  }
+
   /**
    * Moves a route for the bishop if possible, or null if not possible
    * @param requestCoordinates requested movement position [x, y]
    */
   Game_Character.prototype.moveBishop = function(requestCoordinates) {
-    let canMove = (Math.abs(this.x - requestCoordinates[0]) === Math.abs(this.y - requestCoordinates[1]))
-        && !isWall(requestCoordinates);
+    let canMove = this.bishopCanMove(requestCoordinates);
 
     if (canMove) {
       const horizontalMovement = requestCoordinates[0] - this.x;
@@ -294,8 +496,7 @@ function isWall(position) {
    * @param requestCoordinates requested movement position [x, y]
    */
   Game_Character.prototype.moveRook = function(requestCoordinates) {
-    let canMove = (this.x === requestCoordinates[0] || this.y === requestCoordinates[1])
-        && !isWall(requestCoordinates);
+    let canMove = this.rookCanMove(requestCoordinates);
 
     if (canMove) {
       const horizontalMovement = requestCoordinates[0] - this.x;
